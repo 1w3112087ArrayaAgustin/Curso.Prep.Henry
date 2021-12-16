f8b8424ba4bad7cf9b93bc735394a2d0b7ae085e
Arraya Agustin <112087@tecnicatura.frc.utn.edu.ar>
2021-12-16 20:58:25 -0300

    respuesta

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -28,13 +28,12 @@ function numberOfCharacters(string) {
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
   
-  var objeto ={};
+  let objeto ={};
 
   for (let i = 0; i < string.length; i++) {
    if(Object.keys(objeto).includes(string[i])) {
       objeto[string[i]]=objeto[string[i]]+1
       continue
-
    }
    objeto[string[i]]=1;
     
